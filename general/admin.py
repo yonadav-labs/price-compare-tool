9c6e984d59f6dc536772289a91e69b6ab4cc7d7c
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2017-09-06 22:29:41 +1000

    initial commit

---
+++ general/admin.py
@@ -0,0 +1,94 @@
+import os
+import csv
+import pdb
+import datetime
+import mimetypes
+
+from django.contrib import admin
+from django.utils.encoding import smart_str
+from wsgiref.util import FileWrapper
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.admin import SimpleListFilter
+from django.http import HttpResponse
+from django.forms.models import model_to_dict
+
+from .models import *
+
+
+class RunTimeFilter(SimpleListFilter):
+    title = 'RunTime'
+    parameter_name = 'runtime'
+
+    def lookups(self, request, model_admin):
+        return [(ii.id, str(ii.run_at)) for ii in RunTime.objects.all().order_by('-id')]
+
+    def queryset(self, request, queryset):
+        return queryset.filter(run_at=RunTime.objects.all().order_by('-id')[0])
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['sku', 'asin', 'title', 'bb_status', 'our_min_price', 'bb_price', 'bb_price_prev', 'appeagle_strategy', 'num_orders', 'run_at']
+    search_fields = ['sku', 'asin']
+
+    actions = ['export_products']
+    list_filter = (RunTimeFilter,)
+
+    def get_queryset(self, request):
+        qs = super(ProductAdmin, self).get_queryset(request)
+        self.runtime = request.GET.get('runtime')
+        return qs
+
+    def export_products(self, request, queryset):
+        selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
+        ids = ','.join([str(item.id) for item in queryset])
+        fields = [f.name for f in Product._meta.get_fields() 
+                  if f.name not in ['id', 'is_new']]
+
+        path = datetime.datetime.now().strftime("/tmp/.bb_products_%Y_%m_%d_%H_%M_%S.csv")
+        self.write_report(queryset, path, fields)
+        wrapper = FileWrapper( open( path, "r" ) )
+        content_type = mimetypes.guess_type( path )[0]
+
+        response = HttpResponse(wrapper, content_type = content_type)
+        response['Content-Length'] = os.path.getsize( path ) # not FileField instance
+        response['Content-Disposition'] = 'attachment; filename=%s' % smart_str( os.path.basename( path ) ) # same here        
+        return response
+
+    export_products.short_description = "Export products as CSV file" 
+
+    def write_report(self, queryset, path, result_csv_fields):
+        result = open(path, 'w')
+        result_csv = csv.DictWriter(result, fieldnames=result_csv_fields)
+        result_csv.writeheader()
+
+        for product in queryset:
+            product_ = model_to_dict(product, fields=result_csv_fields)
+            for key, val in product_.items():
+                if type(val) not in (float, int, long, bool) and val:
+                    product_[key] = val.encode('utf-8')
+
+            try:
+                result_csv.writerow(product_)
+            except Exception, e:
+                print product_
+
+        result.close()
+
+    def bb_price_prev(self, obj):
+        # pdb.set_trace()
+
+        if self.runtime:
+            prev_obj = Product.objects.filter(asin=obj.asin, run_at_id=self.runtime)
+        else:
+            prev_obj = Product.objects.filter(asin=obj.asin, run_at_id__lt=obj.run_at_id).order_by('-run_at_id')
+
+        if prev_obj:
+            return prev_obj[0].bb_price
+        else:
+            return '-'
+
+    bb_price_prev.short_description = 'Prev BB Price'
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(RunTime)
+admin.site.register(Interval)
---
+++ general/models.py
@@ -0,0 +1,54 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+STRATEGY_CHOICES = (
+    ('38344', 'AN No Tax BB (10%)'),
+    ('38345', 'AN No Tax No BB (10%)'),
+    ('38346', 'AN OS BB (10%)'),
+    ('38347', 'AN OS No BB (10%)'),
+    ('38342', 'AN Tax BB (10%)'),
+    ('38343', 'AN Tax No BB (10%)'),
+    ('38449', 'BB (5% No Tax)'),
+    ('38450', 'BB (5% Tax)'),
+    ('38435', 'CM BB'),
+    ('38436', 'CM No BB'),
+    ('23303', 'Managed Items Strategy (All)'),
+    ('38451', 'No BB (5% No Tax)'),
+    ('38452', 'No BB (5% Tax)'),
+    ('38567', 'OS BB (5%)'),
+    ('38568', 'OS No BB (5%)')
+)
+
+
+class RunTime(models.Model):
+    run_at = models.DateTimeField()
+
+    def __unicode__(self):
+        return str(self.run_at)
+
+
+class Product(models.Model):
+    sku = models.CharField('SKU',max_length=100, null=True, blank=True)
+    asin = models.CharField('ASIN',max_length=100, null=True, blank=True)
+    title = models.CharField('Title', max_length=1000, null=True, blank=True)
+    bb_status = models.BooleanField('BB Status', default=False)
+    bb_owner = models.CharField('Who Has BB', max_length=100, null=True, blank=True)
+    our_min_price = models.FloatField('Our Min Price', null=True, blank=True)
+    bb_price = models.FloatField('BuyBox Price', null=True, blank=True)
+    appeagle_strategy = models.CharField('Appeagle Strategy', max_length=10, choices=STRATEGY_CHOICES, null=True, blank=True)
+    prime_az = models.BooleanField('Prime/AZ', default=False)
+    num_orders = models.IntegerField('No. Of Orders w/in 30 Days ', default=0, null=True, blank=True) 
+    prev_bb_price = models.FloatField('BB Price Previously', null=True, blank=True)
+    run_at = models.ForeignKey(RunTime, verbose_name='Run Time', null=True, blank=True)
+
+    def __unicode__(self):
+        return self.asin
+        
+
+class Interval(models.Model):
+    interval = models.FloatField(help_text="Interval in hour")
+
+    def __unicode__(self):
+        return str(self.interval)
